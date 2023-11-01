96bf4a0b231e2ccc70b7161eafb1ff9450acdbf4
Shanika Lindsay <shanika.m.lindsay@hotmail.co.uk>
2023-11-01 15:43:14 +0000

    navbar iteration

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_treehousetreasures_work', label: 'How TreehouseTreasures Works?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Real maps can be offline',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'We provide a solution for you to be able to use our application when climbing. Offline maps you can use at any time there is no signal at the location',
+    },
+    {
+      title: 'Set an adventure schedule',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Schedule an adventure with friends. On holidays, there are many interesting offers from TreehouseTreasures. That way, there's no more discussion",
+    },
+    {
+      title: 'Technology using augmented reality',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Technology uses augmented reality as a guide if you wish to explore a hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Lots of new locations every month, because we have a worldwide community of glampers who share their best experiences with glamping',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About TreehouseTreasures',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing xixixi', 'Hiking TreehouseTreasures', 'Treehouse Treasures kinthill'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '02034567890' },
+      { label: 'Email Officer', value: 'treehousetreasures@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
